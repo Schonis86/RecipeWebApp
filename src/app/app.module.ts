2f1f8bd60ba1585e207e79b7ee19a21a9e8b47bb
Calle Schönberg <calleschonberg@gmail.com>
2018-09-10 21:41:05 +0200

    Adds page to uppload recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AddRecipeComponent } from './components/private/add-recipe/add-recipe.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddRecipeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/private/add-recipe/add-recipe.component.ts
+++ src/app/components/private/add-recipe/add-recipe.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, NgForm} from "@angular/forms";
+import {Ingredient} from "../../../models/ingredient.model";
+import {Recipe} from "../../../models/recipe.model";
 
 @Component({
   selector: 'app-add-recipe',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-recipe.component.css']
 })
 export class AddRecipeComponent implements OnInit {
+   // currentIngredients: Ingredient[] = [];
+  ingredient: Ingredient;
+  recipe: Recipe = new Recipe();
+  categoryList: string[] = ['Lättlagat', 'Fest', 'Söndagsmiddag', 'MatLåda'];
+  measureList: string[] = ['st', 'gram','kilo', 'cl', 'dl', 'liter', 'knippe'];
+  currentIngredients: Ingredient[] = [
+    {product: 'Torsk', measure: 'g' , qty: 200},
+    {product: 'små Gula lökar', measure: 'st', qty: 10},
+    {product: 'burk hela skalade tomater', measure: 'st', qty: 1},
+    {product: 'fiskbuljong', measure: 'dl', qty: 5},
+    {product: 'burk hela skalade tomater', measure: 'dl', qty: 1}
+  ];
 
-  constructor() { }
+  constructor() {
+      this.recipe = {
+        name: 'Torskgryta med saffran',
+        description: 'En lika smakrik soppa som den är färggrann! De skalade tomaterna ger inte bara färg utan även härlig syra som passar utmärkt ihop med den milda torsken som får koka till sig i grytan. Hintar av vitt vin, saffran och dill får rätten att bli komplett!',
+        category: 'Fest',
+        ingredients: this.currentIngredients,
+        imageUrl: 'https://www.ica.se//icase.azureedge.net/imagevaultfiles/id_73537/cf_259/fiskarhustruns-fisksoppa-003760.jpg'
+      }
+  }
 
   ngOnInit() {
+
+  }
+
+    addRecipe(form: NgForm) {
+
+    console.log( this.recipe.name = form.value.name);
+    this.recipe.name = form.value.name;
+    this.recipe.description = form.value.recipeDescription;
+    this.recipe.imageUrl = form.value.url;
+    this.recipe.category = form.value.category;
+    console.log(this.recipe);
+    form.reset();
+  }
+
+  addIngredient(f: NgForm) {
+
+    this.ingredient = new Ingredient();
+    this.ingredient.product = f.value.product;
+    this.ingredient.qty = f.value.qty;
+    this.ingredient.measure = f.value.measure;
+    this.currentIngredients.push(this.ingredient);
+    f.reset();
   }
 
 }
