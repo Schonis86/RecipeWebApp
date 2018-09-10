a2b3d4c661c390a5b8d351f72adc36ce4d08b686
Calle Schönberg <calleschonberg@gmail.com>
2018-09-10 13:02:46 +0200

    adds components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
