0591a8433aae319923d50682de1fd4069aeec914
Calle Schönberg <calleschonberg@gmail.com>
2018-09-12 00:17:58 +0200

    adds connection to rest pai

--- src/app/services/app-service.ts
+++ src/app/services/app-service.ts
@@ -1,11 +1,16 @@
-import {Injectable} from "@angular/core";
-import {Ingredient} from "../models/ingredient.model";
-import {Recipe} from "../models/recipe.model";
+import {Injectable} from '@angular/core';
+import {Ingredient} from '../models/ingredient.model';
+import {Recipe} from '../models/recipe.model';
+import {Http} from '@angular/http';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class AppService {
 
-   currentIngredients: Ingredient[] = [
+  recipeUrl = 'http://localhost:3000/recipes/';
+
+
+  currentIngredients: Ingredient[] = [
     {product: 'Torsk', measure: 'g', qty: 50},
     {product: 'små Gula lökar', measure: 'st', qty: 2.5},
     {product: 'burk hela skalade tomater', measure: 'st', qty: 0.5},
@@ -20,14 +25,23 @@ export class AppService {
     imageUrl: 'https://www.ica.se//icase.azureedge.net/imagevaultfiles/id_73537/cf_259/fiskarhustruns-fisksoppa-003760.jpg'
   };
 
-  constructor() {
-  }
-  getRecipe():Recipe {
-    return this.recipe
+  constructor(private _http: HttpClient) {
   }
 
+  /*getRecipe():Recipe {
+    return this.recipe;
+  }*/
+
   getIngredient(): Ingredient[] {
-    return this.currentIngredients.slice(0)
+    return this.currentIngredients.slice(0);
+  }
+
+  getRecipe(id) {
+    return this._http.get(this.recipeUrl + id);
+  }
+
+  addRecipe(recipe: Recipe) {
+   return this._http.post(this.recipeUrl, recipe );
   }
 
 
