776f343c398bc76eac10edba5db0c53124d43da6
Calle Schönberg <calleschonberg@gmail.com>
2018-09-12 00:18:27 +0200

    small changes

--- src/app/components/public/recipe/recipe.component.ts
+++ src/app/components/public/recipe/recipe.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {AppService} from "../../../../services/app-service";
-import {Recipe} from "../../../../models/recipe.model";
+import {AppService} from "../../../services/app-service";
+import {Recipe} from "../../../models/recipe.model";
 import {NgForm} from "@angular/forms";
-import {Ingredient} from "../../../../models/ingredient.model";
+import {Ingredient} from "../../../models/ingredient.model";
 
 @Component({
   selector: 'app-recipe',
@@ -13,17 +13,24 @@ export class RecipeComponent implements OnInit {
   recipe: Recipe = new Recipe();
   ingredients: Ingredient[];
   portions: number[] = [2, 4, 6, 8, 10, 12];
-  number: number = 4;
+  number = 4;
 
   constructor(private _service: AppService) {
   }
 
   ngOnInit() {
-    console.log('start');
-    this.recipe = this._service.getRecipe();
+    this.getRecipe('5b981281f9dfe9038f39dbd4');
+    // this.recipe = this._service.getRecipe();
 
   }
   changePortion(value) {
-    this.number = value
+    this.number = value;
   }
+
+  getRecipe(id) {
+    this._service.getRecipe(id).subscribe((data: Recipe) => {
+      this.recipe = data;
+    });
+  }
+
 }
